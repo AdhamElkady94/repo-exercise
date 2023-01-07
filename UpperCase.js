b884d0f101abe801f7e7dd2d4994c8b28c4f6847
AdhamElkady94 <elkady.adham@gmail.com>
2023-01-07 20:33:07 +0200

    final shape

--- UpperCase.js
+++ UpperCase.js
@@ -1,73 +1,31 @@
 
 
-var wars = "The-Stealth-Warrior"
+var wars = "The-Stealth_warrior"
+let i;
+// declaring a loop of i in multiple conditions
+for ((i = wars.indexOf('-')) || (i = wars.indexOf('_')); i < wars.length; i++) {
 
-// declaring a loop of i
-for (let i = wars.indexOf('-'); i < wars.length; i++) {
-
-    // asserting i only equal to "-"
-    if (wars.charAt(i) === '-') {
+    // asserting i only equal to "-" or "_"
+    if ((wars.charAt(i) === '-') || (wars.charAt(i) === '_')) {
         let x = wars.charAt(i);
 
         //identifing the first letter after "-" 
         let cap = wars.charAt(i + 1)
+        // assuring that the targeted letter is not capital
+        if (cap != cap.toUpperCase()) {
+            let char = cap.toUpperCase();
 
-       if (cap != cap.toUpperCase()){
-        let char = cap.toUpperCase();
-
-        // replacing the first letter and "-" with a capital one
-        var wars = wars.replace(`${x + cap}`, `${char}`);
-       }
-       else {
-           var wars = wars.replace(`${x}`,"");
-       };
+            // replacing the first letter and "-" or "_" with a capital one
+            var wars = wars.replace(`${x + cap}`, `${char}`);
+        }
+        else {
+            var wars = wars.replace(`${x}`, "");
+        };
 
 
-        // asserting that there isn't any "-" then excuting the final result 
-        if (wars.indexOf('-') == -1) {
+        // asserting that there isn't any "-" or "_" then excuting the final result 
+        if ((wars.indexOf('-') == -1) && (wars.indexOf('_') == -1)) {
             console.log(wars)
         };
     };
 };
-
-
-
-
-/*
- if (cap === cap.toUpperCase()) {
-            var wars = wars.replace(`${x}`, "");
-        };
-
-else {
-    let char = cap.toUpperCase();
-
-    // replacing the first letter and "-" with a capital one
-    var wars = wars.replace(`${x + cap}`, `${char}`);
-};
-
-
-
-wars.charAt(i + 1) != cap.toUpperCase()
-
-
-
-let x = war.charAt(i);
-
-
-for (let i = war.indexOf('-'); i < war.length; i++) { }
-
-
-if (wars.indexOf('-') == -1){}
-
-
-var arr;
-function logic () {
-    for (var i=arr.indexOf("_")) {
-        return { 
-            arr.indexOf(i+1);
-    }
-        }
-const capitaliza = arr.indexOf(i+1).toUpperCase();
-
-}
-*/
