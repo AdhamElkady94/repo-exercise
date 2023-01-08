3336fdc4b24c4ac577ee5056ca08f9a57e74c523
AdhamElkady94 <elkady.adham@gmail.com>
2023-01-08 19:48:59 +0200

    Final Soluation..

--- UpperCase.js
+++ UpperCase.js
@@ -1,31 +1,57 @@
 
 
-var wars = "The-Stealth_warrior"
-let i;
-// declaring a loop of i in multiple conditions
-for ((i = wars.indexOf('-')) || (i = wars.indexOf('_')); i < wars.length; i++) {
+function toCamelCase(str) {
+    
+    if ((str.includes('-') != true) && (str.includes('_') != true)) {
+        console.log(str);
+    }
+    else {
 
-    // asserting i only equal to "-" or "_"
-    if ((wars.charAt(i) === '-') || (wars.charAt(i) === '_')) {
-        let x = wars.charAt(i);
+        let i;
+        // declaring a loop of i in multiple conditions
+        for ((i = str.indexOf('-') && i != str.indexOf('_')) ||
+            (i = str.indexOf('_') && i != str.indexOf('-'));
+            i < str.length; i++) {
 
-        //identifing the first letter after "-" 
-        let cap = wars.charAt(i + 1)
-        // assuring that the targeted letter is not capital
-        if (cap != cap.toUpperCase()) {
-            let char = cap.toUpperCase();
+            // asserting i only equal to "-" or "_"
+            if ((str.charAt(i) === '-' && str.charAt(i) != '_') ||
+                (str.charAt(i) === '_' && str.charAt(i) != '-')) {
 
-            // replacing the first letter and "-" or "_" with a capital one
-            var wars = wars.replace(`${x + cap}`, `${char}`);
-        }
-        else {
-            var wars = wars.replace(`${x}`, "");
-        };
+                let x = str.charAt(i);
+
+                //identifing the first letter after "-" 
+                let cap = str.charAt(i + 1)
+                // assuring that the targeted letter is not capital
+                if ((cap != cap.toUpperCase())) {
+                    let char = cap.toUpperCase();
+
+                    // replacing the first letter and "-" or "_" with a capital one
+                    str = str.replace(`${x + cap}`, `${char}`);
+                }
+                else {
+                    str = str.replace(`${x}`, "");
+                };
 
 
-        // asserting that there isn't any "-" or "_" then excuting the final result 
-        if ((wars.indexOf('-') == -1) && (wars.indexOf('_') == -1)) {
-            console.log(wars)
+                // asserting that there isn't any "-" or "_" then excuting the final result 
+                if (str.indexOf('-') == -1 && str.indexOf('_') == -1) {
+                    console.log(str);
+                };
+
+            };
+
         };
     };
+    return str;
 };
+
+
+toCamelCase("the-stealth-warrior");
+
+toCamelCase("the_stealth_warrior");
+toCamelCase("the_stealth-warrior");
+
+toCamelCase("the_stealth-Warrior");
+toCamelCase("the-Stealth-Warrior");
+
+toCamelCase("");
